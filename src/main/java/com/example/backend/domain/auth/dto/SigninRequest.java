048a3de3f648a350d9b8afa613fb567971269c75
hwan2-99 <thh1233123@gmail.com>
2025-07-28 08:53:40 +0900

    refactor: 회원가입 및 로그인 필드 수정

--- src/main/java/com/example/backend/domain/auth/dto/SigninRequest.java
+++ src/main/java/com/example/backend/domain/auth/dto/SigninRequest.java
@@ -1,14 +1,12 @@
 package com.example.backend.domain.auth.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class SigninRequest {
 
-    private String username;
+    private String email;
     private String password;
 }
